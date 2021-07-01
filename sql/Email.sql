c1daefb7995f6fd68f9c8227dc2e7c1b7a88b6ee
Soushi888 <Sacha.pignot@protonmail.com>
2021-07-01 03:12:13 -0400

    Update Email.sql

--- sql/Email.sql
+++ sql/Email.sql
@@ -6,12 +6,6 @@ DROP TABLE IF EXISTS email;
 CREATE TABLE email (
   id     int(11) UNSIGNED  NOT NULL AUTO_INCREMENT,
   email  varchar(255)      NOT NULL,
+  type   varchar(255)      NOT NULL   DEFAULT "fournisseur" ,
   PRIMARY KEY (id)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
-
-ALTER TABLE email
-ADD type varchar(255);
-
-UPDATE email
-  SET type = "fournisseur"
